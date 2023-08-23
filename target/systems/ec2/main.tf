e269603cc540f4454da046918216887ff2798903
olusolasp <127486644+olusolasp@users.noreply.github.com>
2023-08-23 14:35:07 -0700

    Update main.tf

--- target/systems/ec2/main.tf
+++ target/systems/ec2/main.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.13.1"
+    }
+  }
+}
 
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "server" {
+  source = "https://github.com/olusolasp/etech-team5-base-iac/tree/main//modules/ec2"
+  number = 5
+}
